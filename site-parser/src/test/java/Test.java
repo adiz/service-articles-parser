10c678bed9b72b92a5885e025868f95b73edc66f
adiz <adi3z@yahoo.com>
2014-03-25 08:57:43 +0200

    Removed ACM_DL parser test object.

--- site-parser/src/test/java/Test.java
+++ site-parser/src/test/java/Test.java
@@ -12,7 +12,6 @@ public class Test {
     public static void main(String[] args) throws IOException {
 
         ArticleParser springer = ArticleParserFactory.getArticleParser("http://link.springer.com/chapter/10.1007/978-3-642-32548-9_30");
-        ArticleParser acmdl = ArticleParserFactory.getArticleParser("");
 
         System.out.println(springer.createArticle());
 
